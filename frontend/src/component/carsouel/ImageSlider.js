7cff06223e0ef6e0807d2c44aa76af759af1d3b6
sauravsuman7 <sauravsrivastava121@gmail.com>
2022-05-09 23:08:59 +0530

    make it better

--- frontend/src/component/carsouel/ImageSlider.js
+++ frontend/src/component/carsouel/ImageSlider.js
@@ -2,12 +2,14 @@ import { Image } from "@chakra-ui/react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
+import "./image.css"
+
 // If you want to use your own Selectors look up the Advancaed Story book examples
 const ImageSlider = ({ slides }) => {
   return (
     <Carousel showThumbs={false} infiniteLoop>
       {slides.map((slide,i) => {
-        return <Image src={slide.url} key={i}/>;
+        return <Image className="imageS" src={slide.url} key={i}/>;
       })}
     </Carousel>
   );
